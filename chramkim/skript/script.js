2e3da0c6dbdb4eb25d0670f8dfd3d059fd4dd631
Roksolana Burbela <55310771+Roksolana-B@users.noreply.github.com>
2023-08-03 11:52:57 +0300

    Add files via upload

---
+++ chramkim/skript/script.js
@@ -0,0 +1,18 @@
+VANTA.CLOUDS({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  speed: 1.50
+})
+VANTA.CLOUDS({
+  el: "#footer",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  speed: 1.50
+})
